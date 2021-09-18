fcc2f10a61a0f3ac45a6868426cd0ad8a3632247
Thomaw <90954106+Thomaw@users.noreply.github.com>
2021-09-18 18:07:12 +0200

    Create Thor.java

---
+++ Easy/Power of Thor - Episode 1/Thor.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+class Player
+{
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in);
+        int lightX = in.nextInt(); // the X position of the light of power
+        int lightY = in.nextInt(); // the Y position of the light of power
+        int initialTX = in.nextInt(); // Thor's starting X position
+        int initialTY = in.nextInt(); // Thor's starting Y position
+
+        while (true)
+        {
+            int remainingTurns = in.nextInt(); // The level of Thor's remaining energy, representing the number of moves he can still make.
+            String direction = "";
+           
+            if (initialTY < lightY)
+            {
+                initialTY++;
+                direction = "S";
+            }
+           
+            else if (initialTY > lightY)
+            {
+                initialTY--;
+                direction = "N";
+            }
+           
+            if (initialTX < lightX)
+            {
+                initialTX++;
+                direction += "E";
+            }
+           
+            else if (initialTX > lightX)
+            {
+                initialTX--;
+                direction += "W";
+            }
+           
+            System.out.println(direction);
+        }
+    }
+
+}
