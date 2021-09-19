822c10f9a328791fba8b6a13b7afa2c40b52b51c
Thomaw <90954106+Thomaw@users.noreply.github.com>
2021-09-19 09:23:00 +0200

    Create Conway_Sequence.py

---
+++ Medium/Conway_Sequence.py
@@ -0,0 +1,26 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+r = int(input())
+l = int(input())
+
+array = [r]
+for i in range(1, l):
+    new_array = []
+    num, last = 0, -1
+    for v in array:
+        if v != last:
+            if num != 0:
+                new_array.append(num)
+                new_array.append(last)
+            num = 1
+        else:
+            num +=1
+        last = v
+    new_array.append(num)
+    new_array.append(last)
+    array = new_array
+print( ' '.join(str(v) for v in array))
